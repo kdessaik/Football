aea1d60cb3c05dd61f15fe43a64231099a837af9
Kambale Dessai <93641789+kdessaik@users.noreply.github.com>
2022-02-09 19:57:11 +0300

    btr

--- style/Javascript/Javascript.js
+++ style/Javascript/Javascript.js
@@ -1,13 +1,14 @@
-function ballon(){ var num =0
+var num =0
+function ballon(){ 
 var h2=document.createElement('h2')
     h2.setAttribute('id','p1')
-var theGoal=document.createTextNode(0)
+var theGoal=document.createTextNode(num)
     h2.appendChild(theGoal)
     var para=document.getElementById('Goal1')
     
     para.appendChild(h2)}ballon()
 function Goal1(){
-    
+    document.getElementById('h2').innerText=num+1
 
     
     
