5d37279b979d62f7da1db0a5c3fec4525e5ec056
Kambale Kibeho <dessaikibeho@gmail.com>
2022-02-09 18:29:28 +0300

    Foot setting
    kdessaik

---
+++ style/Javascript/Javascript.js
@@ -0,0 +1,14 @@
+function Goal1(){
+    
+
+    var h2=document.createElement('h2')
+    document.getElementById('h2').innerText='5'
+    
+    var para=document.getElementById('Goal1')
+    
+    para.appendChild(h2)
+    
+    
+
+    
+}Goal1()
