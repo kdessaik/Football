c0936d38d4d86bce415b4070accddde4f545ea4b
Kambale Dessai <93641789+kdessaik@users.noreply.github.com>
2022-02-09 19:44:10 +0300

    my change

--- style/Javascript/Javascript.js
+++ style/Javascript/Javascript.js
@@ -9,14 +9,7 @@ var theGoal=document.createTextNode(0)
 function Goal1(){
     
 
-    var h2=document.createElement('h2')
-    h2.setAttribute('id','p1')
-    var theGoal=document.createTextNode(0)
-    h2.appendChild(theGoal)
-    var para=document.getElementById('Goal1')
     
-    para.appendChild(h2)
-    document.getElementById('p1').i
     
     
     
