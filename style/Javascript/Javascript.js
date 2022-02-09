865bb79d113a91bd7ed32c45c7da1cc8c6009dc9
Kambale Kibeho <dessaikibeho@gmail.com>
2022-02-09 19:08:34 +0300

    my

--- style/Javascript/Javascript.js
+++ style/Javascript/Javascript.js
@@ -1,14 +1,25 @@
+function ballon(){ var num =0
+var h2=document.createElement('h2')
+    h2.setAttribute('id','p1')
+var theGoal=document.createTextNode(0)
+    h2.appendChild(theGoal)
+    var para=document.getElementById('Goal1')
+    
+    para.appendChild(h2)}ballon()
 function Goal1(){
     
 
     var h2=document.createElement('h2')
-    document.getElementById('h2').innerText='5'
-    
+    h2.setAttribute('id','p1')
+    var theGoal=document.createTextNode(0)
+    h2.appendChild(theGoal)
     var para=document.getElementById('Goal1')
     
     para.appendChild(h2)
+    document.getElementById('p1').i
+    
     
     
 
     
-}Goal1()
+}
